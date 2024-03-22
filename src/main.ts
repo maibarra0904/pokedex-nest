a75ff67b461a039798dbfc23ad4c0cbbaf7f9b97
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-03-21 21:03:26 -0500

    Creando un nuevo pokemon en Mongo

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v2')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Solo permite ingresar la data que se esta esperando, cualquier otro dato del body lo descarta
+      forbidNonWhitelisted: true, //Muestra un mensaje de error en caso de enviar informacion adicional no requerida
+      // transform: true,
+    }),
+  )
+
 
   await app.listen(3000);
 }
